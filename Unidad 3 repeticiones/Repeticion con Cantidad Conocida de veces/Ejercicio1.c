e67502f04d609fc9a9f3c6ce616fcc82050c1a1a
mati-gif <matiassoria7700@gmail.com>
2025-06-17 20:57:53 -0300

    Resolucion de ejercicio 1 repeticiones con for

---
+++ Unidad 3 repeticiones/Repeticion con Cantidad Conocida de veces/Ejercicio1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*1. Imprimir los números del 1 al 10 uno abajo del otro.*/
+
+int main(){
+
+    for(int i = 1 ; i <= 10;i++){
+
+        printf("%d \n",i);
+    }
+    return 0;
+}
